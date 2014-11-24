3b489a2e426e4ead6d81381f50e8f5d2c026ce06
edchen001 <edchen001@gmail.com>
2014-11-24 06:51:54 -0500

    logic to nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,6 @@
 //= require_tree .
 $(document).ready(function() {
 
-
-
-
    $('.banner').unslider({
     speed: 350,
     complete: function() {},
@@ -29,11 +26,6 @@ $(document).ready(function() {
    var unslider = $('.banner').unslider(), 
          data = unslider.data('unslider');
     window.onload = function () {  data.move(0); }
-
-
-
-
-
 
   var submit_button = $('#submit_pre_upload_form');
   var video_upload = $('#video_upload');
@@ -53,54 +45,6 @@ $(document).ready(function() {
   });
 
 
-
-  $('body').scroll(function(){
-    $('#logo').fadeTo("slow" , 0.5,
-      function(){
-        // Animation complete.
-      });
-    });
-
-
-  // $("#create_comment").click(function(e){
-  //   e.preventDefault();
-  //   console.log("linked")
-  //   $("#show_comment_form").toggleClass("hidden")
-  // });
-
-  // $('body').on('click', "#create_comment", function(e){
-  //   e.preventDefault(),
-  //   $link = $(e.target)
-  //   $.ajax({
-  //     url: $link.attr("href"),
-  //     type: $link.attr("method"),
-  //   })
-  //   .done(function(response) {
-  //     $('#show_comment_form').append("<li>" + response + "</li>")
-  //   });
-  // })
-
-  // $('#show_comment_form').on('click', "#comment-submit", function(e){
-  //   e.preventDefault();
-  //   $target = $(e.target)
-  //   $real = $target.parent().parent()
-  //   console.log("linked up")
-  //   $.ajax({
-  //     url: $real.attr("href"),
-  //     type: 'GET',
-  //     dataType: 'json',
-  //   })
-  //   .done(function(response) {
-  //     console.log
-  //     console.log(response)
-  //     console.log("success");
-  //   });
-  // })
 });
 
 
-// $( "#clickme" ).click(function() {
-//   $( "#book" ).fadeTo( "slow" , 0.5, function() {
-//     // Animation complete.
-//   });
-// });
